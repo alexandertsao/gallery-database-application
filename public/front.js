6d83fb0a8dd80791d859bbbe5291ba32305e0c6d
Willis Cao <wcao09@student.ubc.ca>
2023-06-19 02:12:25 -0700

    Implemented input validation and sanitization.

--- public/front.js
+++ public/front.js
@@ -1,3 +1,3 @@
-function visitPage(url){
-    window.location=url;
-}
+// function visitPage(url){
+//     window.location=url;
+// }
--- public/galleries/register.js
+++ public/galleries/register.js
@@ -1,13 +1,17 @@
-import {postToServer, toSQL} from "/public/shared.js";
+import {postToServer, toSQL, sanitize} from "/public/shared.js";
+
+var physical = true;
 
 function radioTypeSelected(e) {
     console.log(e);
     if (this.checked) {
         console.log(this.id);
         if (this.id == 'radio-type-museum' || this.id == 'radio-type-art-gallery') {
+            physical = true;
             document.getElementById("div-physical-group").style.display = "block";
             document.getElementById("div-virtual-group").style.display = "none";
         } else {
+            physical = false;
             document.getElementById("div-physical-group").style.display = "none";
             document.getElementById("div-virtual-group").style.display = "block";
         }
@@ -26,7 +30,35 @@ function setupForm() {
     registerForm.addEventListener("submit", (e) => {
         e.preventDefault();
         
-        // TO DO: handle form submit
+        var validInput = false;
+        // TO DO: validate inputs
+        if (physical) {
+            if (validator.isAlphanumeric(document.getElementById("text-name").value, undefined, {ignore:" -"}) &&
+            validator.isAlphanumeric(document.getElementById("text-address").value, undefined, {ignore:" -"}) &&
+            validator.isAlphanumeric(document.getElementById("text-city").value, undefined, {ignore:" -"}) &&
+            validator.isAlphanumeric(document.getElementById("text-state-province").value, undefined, {ignore:" -"}) &&
+            validator.isAlphanumeric(document.getElementById("text-postal-code").value, undefined, {ignore:" -"}) &&
+            validator.isAlphanumeric(document.getElementById("text-country").value, undefined, {ignore:" -"})) {
+                validInput = true;
+                alert("Input is valid.");
+            } else {
+                alert("Input is invalid.");
+            }
+        } else {
+            if (validator.isAlphanumeric(document.getElementById("text-name").value, undefined, {ignore:" -"}) &&
+            validator.isURL(document.getElementById("text-url").value)) {
+                validInput = true;
+                alert("Input is valid.");
+            } else {
+                alert("Input is invalid.");
+            }
+        }
+
+        if (validInput) {
+            // TO DO: convert form inputs to SQL query
+            
+        }
+
       });
 }
 
