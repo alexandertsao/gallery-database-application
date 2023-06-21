36ced77e3b2d3e7cf89355edcb7e16320efcfd1a
Willis Cao <wcao09@student.ubc.ca>
2023-06-21 03:26:28 -0700

    Fully implemented delete. Fixed bugs.

--- public/shared.js
+++ public/shared.js
@@ -1,15 +1,24 @@
+/**
+ * Sends a query to the SQL server.
+ * Optionally specify callback functions for successes and failures,
+ * with optional arguments.
+ * Query must be in proper format (use toSQL()).
+ * Query must be a single statement (i.e., only one ;).
+ */
 function postToServer(query, callbackSuccess, callbackFail, arg1, arg2) {
 	const xhr = new XMLHttpRequest();
 	xhr.open("POST", "http://localhost:3000/sql");
 	xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
 	xhr.onload = () => {
 	  if (xhr.readyState == 4 && xhr.status == 200) {
-        if (arg1 != undefined) {
-            callbackSuccess(xhr.responseText, arg1);
-        } else if (arg2 != undefined) {
-            callbackSuccess(xhr.responseText, arg1, arg2);
-        } else if (callbackSuccess != undefined) {
-            callbackSuccess(xhr.responseText);
+        if (callbackSuccess != undefined) {
+            if (arg2 != undefined) {
+                callbackSuccess(xhr.responseText, arg1, arg2);
+            } else if (arg1 != undefined) {
+                callbackSuccess(xhr.responseText, arg1);
+            } else {
+                callbackSuccess(xhr.responseText);
+            }   
         }
 	  } else {
         console.log(xhr.status);
@@ -21,21 +30,25 @@ function postToServer(query, callbackSuccess, callbackFail, arg1, arg2) {
 	xhr.send(query);
 }
 
+/**
+ * Sends an multiple SQL queries in the form of an array
+ * in succession, one after another.
+ */
 function multiPostToServer(response, queryArray, callbackFail) {
     // queryArray in the form
     // [{query: "", func: "", arg1: "", arg2: ""}]
     // * must have same callbackFail
     // * func(response, arg1, arg2) is only called on the last query
     if (queryArray.length == 0) return;
-    
+
     alert("multiPostToServer: " + queryArray[0].query);
 
     if (queryArray.length == 1 && queryArray[0].func != undefined) {
         postToServer(toSQL(queryArray[0].query),
                      queryArray[0].func,
                      callbackFail,
-                     query[0].arg1,
-                     query[0].arg2);
+                     queryArray[0].arg1,
+                     queryArray[0].arg2);
     } else {
         postToServer(toSQL(queryArray[0].query), 
                      multiPostToServer, 
@@ -46,17 +59,26 @@ function multiPostToServer(response, queryArray, callbackFail) {
     
 }
 
+/**
+ * Turns a string into JSON SQL object.
+ */
 function toSQL(str) {
     return JSON.stringify({
         sql: str
     })
 }
 
+/**
+ * Function to use with postToServer as callbackFail.
+ */
 function alertDatabaseError(response) {
     alert(response);
 }
 
-// Adapted from https://stackoverflow.com/questions/7744912/making-a-javascript-string-sql-friendly/7760578#7760578
+/**
+ * Adapted from https://stackoverflow.com/questions/7744912/making-a-javascript-string-sql-friendly/7760578#7760578
+ * Escapes all SQL characters in a string.
+ */
 function sanitize(str) {
     return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
         switch (char) {
@@ -84,6 +106,9 @@ function sanitize(str) {
     });
 }
 
+/**
+ * Replaces undefined strings with blank strings.
+ */
 function replaceUndefined(str) {
     if (str == undefined)
         return "";
